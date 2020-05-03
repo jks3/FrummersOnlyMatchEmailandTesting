8ca780802d194ad6fa3d1626d0cae44a2145be9c
Jordan Simkovic <jks3@illinois.edu>
2020-05-03 02:28:07 -0400

    Added random Person generation and match_score tuple generation

--- algo_testing.py
+++ algo_testing.py
@@ -2,6 +2,7 @@ import sys
 import scipy.optimize as sc
 import scipy as scm
 import numpy as np
+import platypus
 
 print(sys.maxsize)
 print(sc.linear_sum_assignment(np.array([[1, 1, 3], [2, 0, 5], [3, 2, 2]])))
@@ -25,14 +26,27 @@ class Match_Tuple:
             {person_a.my_scores[person_b], person_b.my_scores[person_a]})
 
 
-x = Person([3, 10, 8], [.6, .2, .2])
-y = Person([9, 4, 2], [.5, .3, .2])
+#x = Person([3, 10, 8], [.6, .2, .2])
+#y = Person([9, 4, 2], [.5, .3, .2])
 
 boys = []
 girls = []
 
-boys.append(x)
-girls.append(y)
+for i in range(500):
+
+    boy_attributes = np.random.randint(low = 0, high = 10, size = 5)
+
+    boy_weights = np.random.randint(low = 0, high = 10, size = 5)
+    boy_weights = boy_weights/boy_weights.sum()
+
+    girl_attributes = np.random.randint(low=0, high=10, size=5)
+
+    girl_weights = np.random.randint(low=0, high=10, size=5)
+    girl_weights = girl_weights / girl_weights.sum()
+
+    boys.append(Person(boy_attributes, boy_weights))
+
+    girls.append(Person(girl_attributes, girl_weights))
 
 for i in range(len(boys)):
     for j in range(len(girls)):
@@ -42,7 +56,9 @@ for i in range(len(boys)):
 
         a = Match_Tuple(boys[i], girls[j])
 
-print(Match_Tuple.match_tuples[frozenset({y, x})])
+#print(Match_Tuple.match_tuples)
+
+print(Match_Tuple.match_tuples[frozenset({boys[0], girls[0]})])
 
 
 
