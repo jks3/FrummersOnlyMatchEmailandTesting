5a89aff0ce3c1ae4c0b957089401e5034858ef40
Jordan Simkovic <jks3@illinois.edu>
2020-05-03 07:45:47 -0400

    Finished payoff testign

--- algo_testing.py
+++ algo_testing.py
@@ -1,9 +1,15 @@
 import sys
+
+import scipy
+import scipy as scipy
 import scipy.optimize as sc
 import scipy as scm
 import numpy as np
-import platypus
+import platypus.core as ptcore
+import platypus.types as pttypes
+import platypus.algorithms as ptalgo
 import itertools
+import statistics
 
 class Person:
     def __init__(self, my_attributes, my_weights):
@@ -29,7 +35,7 @@ class Match_Tuple:
 boys = []
 girls = []
 
-for i in range(5):
+for i in range(100):
 
     boy_attributes = np.random.randint(low = 0, high = 10, size = 5)
 
@@ -51,21 +57,88 @@ for i in range(len(boys)):
 
         girls[j].my_scores[boys[i]] = girls[j].get_my_score(boys[i].my_attributes)
 
+        if boys[i].my_scores[girls[j]] == girls[j].my_scores[boys[i]]:
+            girls[j].my_scores[boys[i]] = girls[j].my_scores[boys[i]] + .1
+
         a = Match_Tuple(boys[i], girls[j])
 
 #print(Match_Tuple.match_tuples)
 
-print(Match_Tuple.match_tuples[frozenset({boys[0], girls[0]})])
+w, h = 100, 100
 
-all_combinations = []
+payoff_mat = [[0 for x in range(w)] for y in range(h)]
+distance_mat = [[0 for x in range(w)] for y in range(h)]
 
-list1_permutations = itertools.permutations(boys, len(girls))
 
-for each_permutation in list1_permutations:
-    zipped = zip(each_permutation, girls)
-    all_combinations.append(list(zipped))
+#print(list(Match_Tuple.match_tuples[frozenset({boys[400], girls[400]})])[1])
 
-print(all_combinations)
+#print(float(-sys.maxsize))
+
+for i in range(len(boys)):
+    for j in range(len(girls)):
+        #if list(Match_Tuple.match_tuples[frozenset({boys[i], girls[j]})])[0] >= 2 and \
+        #    list(Match_Tuple.match_tuples[frozenset({boys[i], girls[j]})])[1] >= 2:
+            payoff_mat[i][j] = float(list(Match_Tuple.match_tuples[frozenset({boys[i], girls[j]})])[0])
+            + float(list(Match_Tuple.match_tuples[frozenset({boys[i], girls[j]})])[1]) \
+            - abs(float(list(Match_Tuple.match_tuples[frozenset({boys[i], girls[j]})])[0])
+            - float(list(Match_Tuple.match_tuples[frozenset({boys[i], girls[j]})])[1]))
+
+            distance_mat[i][j] = abs(float(list(Match_Tuple.match_tuples[frozenset({boys[i], girls[j]})])[0])
+            - float(list(Match_Tuple.match_tuples[frozenset({boys[i], girls[j]})])[1]))
+        #else:
+        #    payoff_mat[i][j] = -sys.maxsize
+
+payoff_flat = []
+distance_flat = []
+
+for i in range(len(payoff_mat)):
+    for j in range(len(payoff_mat[i])):
+        payoff_flat.append(payoff_mat[i][j])
+        distance_flat.append(distance_mat[i][j])
+
+payoff_stdev = statistics.stdev(payoff_flat)
+payoff_mean = statistics.mean(payoff_flat)
+
+distance_stdev = statistics.stdev(distance_flat)
+distance_mean = statistics.mean(distance_flat)
+
+#print(payoff_stdev)
+
+#print(payoff_mean)
+
+#print(distance_stdev)
+
+#print(distance_mean)
+
+#print(payoff_flat == distance_flat)
+
+for i in range(len(payoff_mat)):
+    for j in range(len(payoff_mat[i])):
+        if payoff_mat[i][j] < payoff_mean or distance_mat[i][j] > distance_mean + distance_stdev:
+            payoff_mat[i][j] = -sys.maxsize
+
+#print(payoff_mat[0])
+
+dom, rang = scipy.optimize.linear_sum_assignment(payoff_mat, maximize = True)
+
+opt_payoffs_vec = []
+
+for i in range(len(dom)):
+    opt_payoffs_vec.append(payoff_mat[dom[i]][rang[i]])
+
+print(opt_payoffs_vec)
+
+print(opt_payoffs_vec.count(-sys.maxsize)/len(opt_payoffs_vec))
+
+#all_combinations = []
+
+#list1_permutations = itertools.permutations(boys, len(girls))
+
+#for each_permutation in list1_permutations:
+#    zipped = zip(each_permutation, girls)
+#    all_combinations.append(list(zipped))
+
+#print(all_combinations)
 
 #truth = []
 
@@ -90,15 +163,3 @@ print(all_combinations)
 #            truth.append(False)
 #print(not (False in truth))
 
-def belegundu(vars):
-    x = vars[0]
-    y = vars[1]
-    return [-2*x + y, 2*x + y], [-x + y - 1, x + y - 7]
-
-problem = platypus.Problem(2, 2, 2)
-problem.types[:] = [platypus.Real(0, 5), platypus.Real(0, 3)]
-problem.constraints[:] = "<=0"
-problem.function = belegundu
-
-algorithm = platypus.NSGAII(problem)
-algorithm.run(10000)
